0ea23e1ca0c81ffa14104cd3acb5c39583b62e19
Wolf-Tungsten <canis.tungsten@foxmail.com>
2019-01-20 00:36:50 +0800

    调整跳转逻辑

---
+++ pages/prepare/_platform/_verifyToken.vue
@@ -0,0 +1,10 @@
+<script>
+import { async } from 'q';
+
+export default {
+  fetch(context){
+    let {platform, verifyToken} = context.params
+    context.redirect(`https://newids.seu.edu.cn/authserver/login?service=http://auth.myseu.cn/verify/${platform}/${verifyToken}`)
+  }
+}
+</script>
